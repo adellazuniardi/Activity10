a661b8b9d711472a92e70fff4cf73f66f058cfbd
adellazuniardi <96398704+adellazuniardi@users.noreply.github.com>
2022-05-26 11:21:05 +0700

    membuat Cpanel

--- app/src/main/java/com/example/activity8/MainActivity.java
+++ app/src/main/java/com/example/activity8/MainActivity.java
@@ -34,7 +34,7 @@ public class MainActivity extends AppCompatActivity {
     private FloatingActionButton fab;
 
     private static final String TAG = MainActivity.class.getSimpleName();
-    private static String url_select = "http://10.0.2.2/umyTI/bacateman.php";
+    private static String url_select = "http://praktikumtiumy.com/bacateman.php";
     private static final String TAG_ID = "id";
     private static final String TAG_NAMA = "nama";
     private static final String TAG_TELPON = "telpon";
